a1e9b75b9df758d4d1fe3aa3bcff0dece4f0c50d
Andrew Edmonds <andrewre23@gmail.com>
2023-08-10 21:24:27 -0400

    added Flask app

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, session, redirect, url_for
+from . import main
+from .forms import NameForm
+from .. import db
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'))
