8f48e2b80fd8196c96ad1a514dc8fc6d3008fc43
Andrew Edmonds <andrewre23@gmail.com>
2023-08-14 00:48:46 -0400

    added extensions

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,10 @@
 from flask_bootstrap import Bootstrap
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_restful import Api
+from flask_praetorian import Praetorian
+from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -12,3 +14,5 @@ db = SQLAlchemy()
 login = LoginManager()
 mail = Mail()
 moment = Moment()
+praetorian = Praetorian()
+toolbar = DebugToolbarExtension()
