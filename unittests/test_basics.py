dd312201da8f1d6af7380f0b46bb0f2b12d2e935
Andrew Edmonds <andrewre23@gmail.com>
2023-08-14 02:28:26 -0400

    added circleCI config

--- unittests/test_basics.py
+++ unittests/test_basics.py
@@ -1,11 +1,13 @@
+import pytest
 import unittest
+
 from flask import current_app
 from app import create_app, db
 
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app('testing')
+        self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
@@ -17,9 +19,11 @@ def tearDown(self):
     self.app_context.pop()
 
 
+@pytest.fixture
 def test_app_exists(self):
     self.assertFalse(current_app is None)
 
 
+@pytest.fixture
 def test_app_is_testing(self):
     self.assertTrue(current_app.config['TESTING'])
